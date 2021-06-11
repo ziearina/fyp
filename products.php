f1360c004cc3060f9381caadff7a321034afb30a
ziearina <78522242+ziearina@users.noreply.github.com>
2021-06-11 09:22:42 +0800

    Update products.php

--- products.php
+++ products.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products</title>
     <link type="text/css" href="nav.css" rel="stylesheet"/>
     <link type="text/css" href="products.css" rel="stylesheet"/>
