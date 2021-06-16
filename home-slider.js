5dfc9b2e40000c8c2e4b0117bbe59f9c38caa6c1
Zeetta <78522242+ziearina@users.noreply.github.com>
2021-06-16 17:34:20 +0800

    Update home-slider.js

--- home-slider.js
+++ home-slider.js
@@ -45,5 +45,5 @@ function slidefun(n) {
     }
     imgslide[count - 1].style.display = "block";
     dot[count - 1].classList.add('active');
-    timer = setTimeout(autoslide, 400);
+    timer = setTimeout(autoslide, 4000);
 }
